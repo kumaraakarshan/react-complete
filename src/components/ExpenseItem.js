d71de36268ab2e8733e4df714c3cc68c6567492b
kumaraakarshan <aakarshan0005@gmail.com>
2023-09-08 16:25:01 +0530

    location is added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,6 +7,7 @@ function ExpenseItem(props) {
             <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
+                <h2>{props.location}</h2>
                 <div className='expense-item__price'>Rs { props.amount}</div>
             </div>
         </div>
