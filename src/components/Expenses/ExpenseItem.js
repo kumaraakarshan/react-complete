4298f7b8bdf3fdaf190530a7410d6bf5b224efdf
kumaraakarshan <aakarshan0005@gmail.com>
2023-09-10 13:20:45 +0530

    expense amount updated to rs 100

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,11 +7,21 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
   // function clickHandler() {}
   const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
   console.log('ExpenseItem evaluated by React');
   
   const clickHandler = () => {
     setTitle('Updated!');
     console.log(title);
+  };
+  const DeleteHandler = () => {
+    setTitle('Deleted!');
+    console.log(title);
+  };
+  const amountHandler = () => {
+    setAmount('100');
+    console.log(amount);
+   
   };
 
   return (
@@ -19,10 +29,11 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>Rs{amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={clickHandler}>Delete</button>
+      <button onClick={amountHandler}>Change Amount</button>
+      <button onClick={DeleteHandler}>Delete</button>
     </Card>
   );
 }
